8809c05831de1f7c5e0bcc257c541c67ec21b8a1
Michael Lee <michael.lee.0x2a@gmail.com>
2013-11-23 20:37:21 -0800

    Added basic ranking code

--- download-data.php
+++ download-data.php
@@ -4,23 +4,59 @@ require_once('utils.php');
 
 $conn = open_database_connection($config);
 
-//$user_data = grab_data($conn, 'Users');
-//$mentor_data = grab_data($conn, 'Mentors');
+$user_data = grab_data($conn, 'Users');
+$mentor_data = grab_data($conn, 'Mentors');
 
+$similarities = match_mentors($user_data, $mentor_data, 4);
+
+function match_mentors($users, $mentors, $threshold) {
+    $data = array();
+    foreach ($users as $user) {
+        $ranked_mentors = rank_mentors($user, $mentors);
+        $data[$user['index']] = array(
+            'user' => $user,
+            'mentors' => rank_mentors($user, $mentors));
+    }
+    return $data;
+}
+
+
+function cmp_similarity($a, $b) {
+    return $a['score'] - $b['score'];
+}
+    
+function rank_mentors($user, $mentors) {    
+    $data = array();
+    foreach ($mentors as $mentor) {
+        array_push($data, 
+            array(
+                'score' => get_similarity_score($user, $mentor),
+                'mentor' => $mentor));
+    }
+    
+    uasort($data, 'cmp_similarity');
+    
+    return $data;
+}
+
+function get_similarity_score($person1, $person2) {
+    $intersection = array_intersect($person1['keywords'], $person2['keywords']);
+    return count($intersection);
+}
 
 
 function grab_data($conn, $table_name) {
     $result = $conn->query("select * from {$table_name};");
-    $result->data_seek(0);
     $output = array();
-    while ($row = $result->fetch_assoc()) {
+    foreach ($result as $row) {
         $output[$row['index']] = array(
+            'index' => $row['index'],
             'name' => $row['name'],
             'email' => $row['email'],
             'location' => $row['location'],
             'field_of_interest' => $row['foi'],
             'keywords' => explode(',', $row['keywords']));
-    }   
+    }
     
     return $output;
 }
@@ -35,10 +71,13 @@ function grab_data($conn, $table_name) {
     <p><?php echo $config['database_name'] ?></p>
     <p><?php echo $config['database_password'] ?></p>
     <h2>User</h2>
-    <p><?php echo var_dump($user_data) ?></p>
+    <pre><?php echo var_dump($user_data) ?></pre>
     
     <h2>Mentor</h2>
-    <p><?php echo var_dump($mentor_data) ?></p>
+    <pre><?php echo var_dump($mentor_data) ?></pre>
+    
+    <h2>Matches</h2>
+    <pre><?php echo var_dump($similarities) ?></pre>
 </body>
 </html>
 
