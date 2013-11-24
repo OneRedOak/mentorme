d8eea500209c53d08f5093ca050ad8301623a69b
unknown <daltoncaughell@gmail.com>
2013-11-23 19:49:43 -0800

    Fixed mysql driver

--- utils.php
+++ utils.php
@@ -1,10 +1,6 @@
 <?php
 	function open_database_connection($config) {
-	    $conn = mysqli_connect(
-	        $config['database_host'],
-	        $config['database_username'],
-	        $config['database_password'],
-	        $config['database_name']);
-	    return $conn;
+	  	return new PDO('mysql:host=' . $config['database_host'] . ';dbname=' . $config['database_name'], 
+	  					$config['database_username'], $config['database_password']);
 	}
 ?>
